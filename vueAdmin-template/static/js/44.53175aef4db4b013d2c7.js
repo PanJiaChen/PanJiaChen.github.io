a10df8d80c99e01354c91f25195e64224643a5a7
Pan <panfree23@gmail.com>
2017-07-03 14:50:07 +0800

    update

--- vueAdmin-template/static/js/44.53175aef4db4b013d2c7.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([44],{317:function(e,n,o){var i=o(2)(o(624),null,null,null,null);e.exports=i.exports},624:function(e,n,o){"use strict";Object.defineProperty(n,"__esModule",{value:!0}),n.default={name:"authredirect",created:function(){var e=window.location.search.slice(1);window.opener.location.href=window.location.origin+"/login#"+e,window.close()}}}});
-//# sourceMappingURL=44.53175aef4db4b013d2c7.js.map
